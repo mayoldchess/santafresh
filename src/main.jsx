7e28a5b19f314f0d17370c5a8b721cc05835f164
mayoldchess <MAY.OLDCHESS@YAHoO.COM>
2025-10-01 23:03:19 +0200

    Elf Voice UI: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,4 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')).render(
-  <div>
-    <h1>Elf Chat setup</h1>
-    <p>Your site is deployed by GitHub Pages. Once your Worker is up, paste its URL here in your full UI build.</p>
-  </div>
-)
+import App from './App.jsx'
+createRoot(document.getElementById('root')).render(<App />)
